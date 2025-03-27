2a96be3b5427ccb4673df612c7246414ea5c9474
Ten <03822007sasha@gmail.com>
2025-03-27 09:07:53 +0300

    Now the code in the main.py module is commented out and basic logging of the start of the synchronisation process and the fact that the server is running is done

--- main.py
+++ main.py
@@ -5,20 +5,43 @@ from ten_utils.log import Logger
 from common.constants import (
     ENV_MODE,
 )
-from service.music import get_all_music
+from sync.database import sync_db_table_tracks
 
 
-logger = Logger(__name__, level=4)
+logger = Logger(__name__, level=0)
 
 
-def run_sync():
+def run_sync() -> None:
+    """
+    Функция для запуска синхронизации.
+    Используется и нужна для точки входа программы.
+    :return: None
+    """
+    logger.debug(f"The mode of operation is set: {ENV_MODE}")
+    logger.info("Synchronisation server started!")
+
+
     if ENV_MODE == "dev":
+        """
+        При запуске в режиме разработки, 
+        скрипт синхронизации запускается каждые 10 сек
+        """
         while True:
+            logger.info("Start synchronization of track paths...")
+            sync_db_table_tracks()
+            logger.info("Finish synchronization of track paths!")
+
             time.sleep(10)
-            pass
 
     elif ENV_MODE == "prod":
-        pass
+        """
+        При запуске в режиме продакшена,
+        скрипт синхронизации запускается с помощью cron
+        внутри docker образа
+        """
+        logger.info("Start synchronization of track paths...")
+        sync_db_table_tracks()
+        logger.info("Finish synchronization of track paths!")
 
     else:
         logger.critical("ENV environment variable is not set")
